3f199d128916e39f6450dd9c89d20a6143ec90ac
allanDev9 <allanpenaguerra2003@gmail.com>
2024-05-25 15:21:21 -0600

    Diseño de la pagina

---
+++ index.js
@@ -0,0 +1,18 @@
+const Range = document.getElementById("range");
+const ageDisplay = document.getElementById("age");
+
+range.addEventListener("input", function(){
+    const age = range.value;
+    ageDisplay.textContent = age;
+});
+
+const Title = document.querySelector(".title");
+
+const colors = ["black", "red","aqua", 
+                "brown","green", "blue", "purple"];
+let colorIndex = 0;
+
+function cambiarColor(){
+    colorIndex = (colorIndex + 1) % colors.length;
+    Title.style.color = colors[colorIndex];
+}
